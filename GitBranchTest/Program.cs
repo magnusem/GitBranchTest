87ab0a602869b69c30c638745913f931adb1645e
Magnus Emil Mortensen <magn1596@edu.easj.dk>
2016-11-08 10:53:04 +0100

    Console.WriteLine("Er nu på develop branch");

--- GitBranchTest/Program.cs
+++ GitBranchTest/Program.cs
@@ -11,6 +11,7 @@ namespace GitBranchTest
         static void Main(string[] args)
         {
             Console.WriteLine("Test af Git Branch");
+            Console.WriteLine("Er nu på develop branch");
         }
     }
 }
